f84e8717e39e89f2b97f74f64807b68f4a55d17e
Mohammed Issam Kassel <kassel.issam@gmail.com>
2021-04-02 18:20:00 +0200

    affichage

--- IDAW-projet/backend/aliments.php
+++ IDAW-projet/backend/aliments.php
@@ -5,8 +5,12 @@ try{
                 $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 
 
-                $infosUtilisateur = $dbco->prepare("SELECT * FROM `aliments` 
-                LIMIT 5");
+                $infosUtilisateur = $dbco->prepare("SELECT Nom, Type, Pro.Ratio AS Proteines, Glu.Ratio As Glucides, Lip.Ratio As Lipides, Suc.Ratio As Sucres FROM `aliments` 
+                LEFT JOIN a_pour_apport AS Pro ON aliments.ID_aliments = Pro.ID_aliments AND Pro.ID_apport=1
+                LEFT JOIN a_pour_apport AS Glu ON aliments.ID_aliments = Glu.ID_aliments AND Glu.ID_apport=2
+                LEFT JOIN a_pour_apport AS Lip ON aliments.ID_aliments = Lip.ID_aliments AND Lip.ID_apport=3
+                LEFT JOIN a_pour_apport AS Suc ON aliments.ID_aliments = Suc.ID_aliments AND Suc.ID_apport=4
+                ");
                 $infosUtilisateur->execute();
                 
 
@@ -17,4 +21,3 @@ try{
                 echo "Erreur : " . $e->getMessage();
             }
             ?>
-
